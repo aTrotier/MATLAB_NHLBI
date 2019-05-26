bd6e409fa4f07e1d726cb823e557ef21104d43ef
Ramasawmy <ramasawmyr@nih.gov>
2019-05-26 18:35:28 -0400

    self-gate with PCA, not tested with recon

--- recon_toolbox/self_gate_DC.m
+++ recon_toolbox/self_gate_DC.m
@@ -1,148 +1,187 @@
-function [sg_info] = self_gate_DC(dfile, nfile, phases, TR)
+function [sg_info] = self_gate_DC(kspace_pw, i_rd, phases)
+% Stand-alone test function
 % [sg_info] = self_gate_DC(dfile, nfile, phases [#cardiac <30> #resp <1>])
+% This does a lot of smoothing for peak detection..
+% PCA = 1 for motion 
+
 if nargin < 3
-   phases = [30 1]; 
-%    TR = 0.0186;
+    phases = [30 1];
 end
-
-raw_data = h5read(dfile,'/dataset/data');
-i_rd = read_h5_header(dfile);
-TR = 2 * 1e-3 * i_rd.sequenceParameters.TR;
-
 cardiac_frames = phases(1);
 resp_phases = phases(2);
 
-dmtx = ismrm_dmtx_RR(nfile);
-
-channels = raw_data.head.available_channels(1);
-samples = raw_data.head.number_of_samples(1);
-
-%%
-clear data;
-for i = 1:length(raw_data.data)
-    d = complex(raw_data.data{i}(1:2:end), raw_data.data{i}(2:2:end));
-    d = reshape(d, samples, 1, channels);
-    data(i,:) = double(squeeze(d(1,1,:)));
+% nasty switch for compatibility
+if ischar(kspace_pw) && ischar(i_rd) % assume file names are being fed in
+    raw_data = h5read(kspace_pw,'/dataset/data');
+    dmtx = ismrm_dmtx_RR(i_rd);
+    
+    i_rd = read_h5_header(kspace_pw);
+    TR = 1e-3 * i_rd.sequenceParameters.TR;
+    
+    
+    channels = raw_data.head.available_channels(1);
+    samples = raw_data.head.number_of_samples(1);
+    
+    clear data;
+    for i = 1:length(raw_data.data)
+        d = complex(raw_data.data{i}(1:2:end), raw_data.data{i}(2:2:end));
+        d = reshape(d, samples, 1, channels);
+        data(i,:) = double(squeeze(d(1,1,:)));
+    end
+    data = ismrm_apply_noise_decorrelation_mtx(data, dmtx);
+else
+    channels = size(kspace_pw,ndims(kspace_pw));
+    
+    % take first data point
+    S.subs = repmat({':'},1,ndims(kspace_pw)); % handle 2D/3D images
+    S.subs{1} = 1;
+    S.type = '()';
+    data = subsref(kspace_pw, S);
+    
 end
-data = ismrm_apply_noise_decorrelation_mtx(data, dmtx);
-% figure, plot(real(data))
+
+
+%% Prep data and PCA (1-channel out)
+
+data = abs(data);
+% figure, plot(data)
 
 cmp = jet(single(channels));
-for i = 1:channels
-    leg_chan{i} = num2str(i);
+channel_legend = cellstr(num2str([1:channels]'));
+
+% average two flow sets together for even phase-contrast binning
+if i_rd.encoding.encodingLimits.set.maximum
+    TR = (i_rd.encoding.encodingLimits.set.maximum + 1)*TR;
+    data = squeeze(mean(reshape(data, [2 length(data)/2 channels]),1));
+    % figure, plot(data)
 end
-% 
-% figure, hold on,
-% for i = 1:channels
-%     temp = mean(reshape(real(data(:,i)), [2 length(data)/2]),1);
-%     plot(smooth(real(temp)))
+
+% Take SVD
+[U,S,V] = svd(data,'econ');
+% figure, plot(diag(S))
+
+% % Rank Contribution [ academic : if you want to see which rank of motion data]
+% rank_matrix = zeros(size(U));
+% for i = 1:length(S)
+%     rank_matrix(:,i) = U(:,i)*S(i,i)*V(i,i)';
 % end
+% figure, colorful_plots(rank_matrix)
+%
+% figure, clear P_out D_out;
+% for i = 1:channels
+%     subplot(1,2,1); hold on;
+%     plot(smooth(rank_matrix(:,i)), '-', 'Color', cmp(i,:))
+%
+%     subplot(1,2,2); hold on;
+%     Y = fft(smooth( rank_matrix(:,i) ));
+%     P2 = abs(Y/length(U));
+%     P1 = P2(1:(length(U))/2+1);
+%     P1(2:end-1) = 2*P1(2:end-1);
+%     f = (1/TR)*(0:((length(U))/2))/(length(U));
+%     plot(f, P1, '-', 'Color', cmp(i,:))
+% %     P_out(:,i) = P1;
+% end
+% xlim([0.1 3.6])
+% legend(channel_legend)
 
-figure, clear P_out D_out;
-for i = 1:channels
-    temp = mean(reshape(abs(data(:,i)), [2 length(data)/2]),1);
-    D_out(:,i) = temp;
+
+% PCA
+channels_out = 1;
+data_pca = U(:,1:channels_out)*S(1:channels_out,1:channels_out)*V(1:channels_out,1:channels_out)';
+% figure, plot(data_pca)
+
+smooth_data = 1;
+if smooth_data
+    for i = 1:channels_out
+        data_pca(:,i) = smooth(data_pca(:,i));
+    end
+end
+
+%% Fourier transform and visualise motion frequencies
+figure, clear P_out;
+for i = 1:channels_out
     subplot(1,2,1); hold on;
-    plot(smooth(real(temp)), '-', 'Color', cmp(i,:))
-
+    plot(data_pca(:,i), '-', 'Color', cmp(i,:))
+    
     subplot(1,2,2); hold on;
-    Y = fft(smooth(real(temp)));
-    P2 = abs(Y/length(temp));
-    P1 = P2(1:(length(temp))/2+1);
+    Y = fft(data_pca);
+    P2 = abs(Y/length(U));
+    P1 = P2(1:(length(U))/2+1);
     P1(2:end-1) = 2*P1(2:end-1);
-    f = (1/TR)*(0:((length(temp))/2))/(length(temp));
-    plot(f, P1, '-', 'Color', cmp(i,:))
+    freq_range = (1/TR)*(0:((length(U))/2))/(length(U));
+    plot(freq_range, P1, '-', 'Color', cmp(i,:))
     P_out(:,i) = P1;
 end
-xlim([0.1 3.6])
-legend(leg_chan)
+xlim([0.1 3.6]); xlabel('Freq (Hz)');
+legend(channel_legend)
 
-sg_info.dc_data = D_out;
+sg_info.dc_data = data_pca;
 sg_info.dc_fft = P_out;
 
 %% CARDIAC binning
-tempf = find(0.5 < f & f < 3.5); % MIN/MAX HEART RATE = 30/210 bpm
+tempf = find(0.5 < freq_range & freq_range < 3.5); % MIN/MAX HEART RATE = 30/210 bpm
 P_temp = P_out(tempf, :);
 [ty, coil_ecg] = find(P_temp == max(P_temp(:)));
-freq_ecg = f(ty+min(tempf)-1);
+freq_ecg = freq_range(ty+min(tempf)-1);
 
 pass_band_ecg = P_out(:,coil_ecg);
 px = (1:length(P_out));
 px(tempf) = [];
-dyn_cardiac = smooth(D_out(:,coil_ecg));
+dyn_cardiac = smooth(data_pca(:,coil_ecg));
 
 figure, plot(dyn_cardiac, 'k-'); hold on;
 d_inter = round((1/freq_ecg)/TR)+5;
 if(~mod(d_inter,2))
     d_inter = d_inter+1;
 end
-ave_cardiac = smooth(D_out(:,coil_ecg), d_inter);
+ave_cardiac = smooth(data_pca(:,coil_ecg), d_inter);
 
 plot(ave_cardiac, 'r-')
 legend({'Smoothed ECG + RESP','Smoothed RESP'});
 
 % Peaks
-[pks, locs] = findpeaks(smooth(D_out(:,coil_ecg)-smooth(D_out(:,coil_ecg), d_inter)), 'MinPeakDistance',round(0.75* (1/freq_ecg)/TR) );
-% Rising edge
-[pks2, locs2] = findpeaks(smooth(diff(smooth(D_out(:,coil_ecg)-smooth(D_out(:,coil_ecg), d_inter)))), 'MinPeakDistance',round(0.75* (1/freq_ecg)/TR) );
-[mean(diff(locs)) mean(diff(locs2)); std(diff(locs)) std(diff(locs2)) ]
+[pks, locs] = findpeaks(smooth(data_pca(:,coil_ecg)-smooth(data_pca(:,coil_ecg), d_inter)), 'MinPeakDistance',round(0.75* (1/freq_ecg)/TR) );
 
-          %% compare rising edge and peaks
-          
-%           figure, hold on,
-%           x1 = smooth(D_out(:,coil_ecg)-smooth(D_out(:,coil_ecg), d_inter));
-%           plot(x1./max(x1(:)), 'b-');
-%           plot(locs, pks/max(x1(:)), 'bo');
-%           x2 = smooth(diff(smooth(D_out(:,coil_ecg)-smooth(D_out(:,coil_ecg), d_inter))));
-%           plot(x2./max(x2(:)), 'r-');
-%           plot(locs2, pks2/max(x2(:)), 'ro');
-%           plot(smooth(D_out(:,coil_ecg))/max(D_out(:,coil_ecg)), 'k-')
-           
+% remove steady-state descent
+steady_state_inds = find(diff(ave_cardiac)  > median(diff(ave_cardiac)) - std(diff(ave_cardiac)));
+steady_state_start = steady_state_inds(1);
+steady_state_inds = find(locs > steady_state_start);
+pks = pks(steady_state_inds);
+locs = locs(steady_state_inds);
+
     %% Bin each cardiac cycle
-    rpd_y = pks; rpd_x = locs; 
-% cardiac frames
+rpd_y = pks; rpd_x = locs;
+
+iRRvec = [[rpd_x(1:end-1)'];rpd_x(2:end)'];
+
+bin_data1 = zeros(1,length(data_pca));
+
+%     figure; hold on;
+for iRR = 1:length(rpd_x)-1
+    RRvec = iRRvec(1, iRR):iRRvec(2, iRR);
     
-    iRRvec = [[0 rpd_x(1:end-1)']+1;rpd_x'];
-
-    bin_data1 = zeros(1,rpd_x(end));
-%     figure;
-    for iRR = 1:length(rpd_x)
-% %         RRvec = iRRvec(1, iRR):iRRvec(2, iRR);
-% %         temp = dyn_cardiac(RRvec);
-% %         bin_step = range(temp)/(cardiac_frames);
-% %
-% %         bin_data2 = 1+floor((temp-min(temp))/(bin_step*1.001));
-% %
-% %         bin_data1(RRvec) = bin_data2';
-
-% simpler method, ?? convert to "rpd"??
-         RRvec = iRRvec(1, iRR):iRRvec(2, iRR);
-%       RRvec2 = floor((1:(length(21:32)*100))/(1.001*(32 - 21)/(cardiac_frames-1)))
-%          bin_data2 = floor( (1:length(RRvec)) / (1.001*(iRRvec(2, iRR) - iRRvec(1, iRR))/(cardiac_frames-1)) );
-         temp_vec =  1:length(RRvec);
-         bin_data2 = zeros(size(temp_vec)); bin_data2(1) = 1; bin_data2(end) = cardiac_frames;
-         bin_data2(2:length(RRvec)-1) =   round( temp_vec(1:end-2)*( 1.001*cardiac_frames/(length(RRvec)-1)) );
-      bin_data1(RRvec) = bin_data2';
-
-%                 plot(bin_data2);
-    end
+    temp_vec =  1:length(RRvec);
+    bin_data2 = zeros(size(temp_vec)); bin_data2(1) = 1; bin_data2(end) = cardiac_frames;
+    bin_data2(2:length(RRvec)-1) =   round( temp_vec(1:end-2)*( 1.001*cardiac_frames/(length(RRvec)-1)) );
+    bin_data1(RRvec) = bin_data2';
+%         plot(bin_data2);
+end
 
 
     %% export
 
-    sg_info.ecg_binned = bin_data1;
-    sg_info.ecg_freq = freq_ecg;
-    sg_info.ecg_chan = coil_ecg;
+sg_info.ecg_binned = bin_data1;
+sg_info.ecg_freq = freq_ecg;
+sg_info.ecg_chan = coil_ecg;
 
 
-  
+%% RESPIRATORY binning
 
-%% RESP binning
-tempf = find(min(f(f>0)) < f & f < 0.5); % MIN/MAX RESP RATE = ~0/30 bpm
+tempf = find(min(freq_range(freq_range>0)) < freq_range & freq_range < 0.5); % MIN/MAX RESP RATE = ~0/30 bpm
 
 P_temp = P_out(tempf, :);
 [ty, coil_resp] = find(P_temp == max(P_temp(:)));
-freq_resp = f(ty+min(tempf)-1);
+freq_resp = freq_range(ty+min(tempf)-1);
 
 pass_band_resp = P_out(:,coil_resp);
 px = (1:length(P_out));
@@ -152,219 +191,58 @@ d_inter2 = round((1/freq_resp)/TR)+5;
 if(~mod(d_inter2,2))
     d_inter2 = d_inter2+1;
 end
-dyn_resp = smooth(D_out(:,coil_resp), d_inter);
-ave_resp = smooth(smooth(D_out(:,coil_resp),d_inter2));
+dyn_resp = smooth(data_pca(:,coil_resp), d_inter);
+ave_resp = smooth(smooth(data_pca(:,coil_resp),d_inter2));
 
 figure, plot(dyn_resp, 'k-'); hold on;
 plot(ave_resp, 'r-'); plot(dyn_resp - ave_resp + median(ave_resp), 'b--')
 legend({'Smoothed RESP','Ave RESP'});
 
-    % Peaks detection
-    [pks, locs] = findpeaks(dyn_resp-ave_resp, 'MinPeakDistance',round(0.75* (1/freq_resp)/TR) );
-    % Rising edge
-    [pks2, locs2] = findpeaks(smooth(diff( smooth( dyn_resp-ave_resp ))), 'MinPeakDistance',round(0.75* (1/freq_resp)/TR) );
-%     [[mean(diff(locs)) mean(diff(locs2))]; [std(diff(locs)) std(diff(locs2))]]
+% Peaks detection
+[pks, locs] = findpeaks(dyn_resp-ave_resp, 'MinPeakDistance',round(0.75* (1/freq_resp)/TR) );
+steady_state_inds = find(locs > steady_state_start);
+pks = pks(steady_state_inds);
+locs = locs(steady_state_inds);
 
-  %%
-% 
-%     figure, hold on,
-%     x1 = dyn_resp-ave_resp;
-%     plot(x1./max(x1(:)), 'b-');
-%     plot(locs, x1(locs), 'bo');
-%     x2 = smooth(diff( smooth( dyn_resp-ave_resp )));
-%     plot(x2./max(x2(:)), 'r-');
-%     plot(locs2, x2(locs), 'ro');
-%     plot(smooth(D_out(:,coil_resp) - min(D_out(:,coil_resp)))/(max(D_out(:,coil_resp)) - min(D_out(:,coil_resp))), 'k-')
-% 
 
     %% Respiratory phases
+
+    rpd_y = pks; rpd_x = locs;
+    iRRvec = [[rpd_x(1:end-1)'];rpd_x(2:end)'];
     
-    % remove non-steady state signal (0.5 = aggressive)
-%     ss_start = find(ave_resp < 1/2*std(ave_resp) + median(ave_resp), 1, 'first');
-%     dyn_resp = dyn_resp(ss_start:end);
-%     ave_resp = ave_resp(ss_start:end);
-%     figure, plot(dyn_resp)
-
- rpd_y = pks; rpd_x = locs; 
- iRRvec = [[0 rpd_x(1:end-1)']+1;rpd_x'];
-
-    bin_data1 = zeros(1,rpd_x(end));
-%     figure;
-    for iRR = 1:length(rpd_x)
-
-         RRvec = iRRvec(1, iRR):iRRvec(2, iRR);
-%       RRvec2 = floor((1:(length(21:32)*100))/(1.001*(32 - 21)/(cardiac_frames-1)))
-%          bin_data2 = floor( (1:length(RRvec)) / (1.001*(iRRvec(2, iRR) - iRRvec(1, iRR))/(cardiac_frames-1)) );
-         temp_vec =  1:length(RRvec);
-         temp_resp = dyn_resp - ave_resp;
-         resp_binned = floor((temp_resp(RRvec)-min(temp_resp(RRvec)))/(1.001*range(temp_resp(RRvec))/resp_phases));
-         bin_data1(RRvec) = resp_binned' + 1;
-
-%                 plot(bin_data2);
+    bin_data1 = zeros(1,length(data_pca));
+    
+    % figure, hold on;
+    for iRR = 1:length(rpd_x)-1
+        
+        RRvec = iRRvec(1, iRR):iRRvec(2, iRR);
+        temp_vec =  1:length(RRvec);
+        temp_resp = dyn_resp - ave_resp;
+        resp_binned = floor((temp_resp(RRvec)-min(temp_resp(RRvec)))/(1.001*range(temp_resp(RRvec))/resp_phases));
+        bin_data1(RRvec) = resp_binned' + 1;
+        
+        %     plot(resp_binned);
     end
-
-%     resp_binned = floor((dyn_resp-min(dyn_resp))/(range(dyn_resp)/resp_phases));
-%     resp_binned(resp_binned==resp_phases) = resp_phases-1;
 
 
     %% export
-sg_info.resp_binned = bin_data1;
-sg_info.resp_freq = freq_resp;
-sg_info.resp_chan = coil_resp;
+    
+    sg_info.resp_binned = bin_data1;
+    sg_info.resp_freq = freq_resp;
+    sg_info.resp_chan = coil_resp;
+    
+    figure, hold on, plot(sg_info.resp_binned, 'r-');   plot(sg_info.ecg_binned /(cardiac_frames/resp_phases) , 'b-');
 
-figure, hold on, plot(sg_info.resp_binned, 'r-');   plot(sg_info.ecg_binned /(cardiac_frames/resp_phases) , 'b-');   
+%% Export pad output for 2-sets
+if i_rd.encoding.encodingLimits.set.maximum
+    temp = sg_info.resp_binned;
+    sg_info.resp_binned = reshape(repmat(temp, [2 1]), [1 length(temp)*2]);
+    
+    temp = sg_info.ecg_binned;
+    sg_info.ecg_binned = reshape(repmat(temp, [2 1]), [1 length(temp)*2]);
+end
 
 
-%% ?/??/???
-% temp_data = data(201:600,:);
-% figure, plot(abs(temp_data))
-% [u, s, v] = svd(temp_data, 'econ');
-% figure, plot(abs(u))
-
- 
-%% PCA - Liheng Guo <need to optimise implementation>
-
-% Guo_flag  = 0;
-% if Guo_flag
-%     figure,
-%     for i = 1:5
-%         eigVecs = LG_eigNavTrace(D_out, i);
-%         subplot(5,2,2*i -1), plot(smooth(eigVecs(:,1)));
-%         subplot(5,2,2*i), plot(smooth(eigVecs));
-%     end
-%     
-%     eigVecs = LG_eigNavTrace(D_out, 8);
-% [LG_cardiac, cardiac_cycles] = LG_CardCurve_eig(eigVecs, TR*1000, [50 210]);
-% % [LG_cardiac, cardiac_cycles] = LG_CardCurve_eig(eigVecs(200:end,:), TR*1000, [100 210]);
-% [LG_resp, resp_cycles] = LG_CardCurve_eig(eigVecs, TR*1000, [min(f(f>0))*60 30]);
-% cardiac_freq  = cardiac_cycles/(TR*length(LG_cardiac));
-% resp_freq  = resp_cycles/(TR*length(LG_cardiac));
-% figure, plot(smooth(LG_cardiac), 'r-'); hold on, plot(smooth(LG_resp), 'b-')
-% figure, plot(smooth(D_out(:,coil_ecg)), 'r-'), hold on, plot(smooth(D_out(:,coil_resp)), 'b-')
-% 
-%     
-% end
-% 
-% 
-% [p1, l1] = self_gate_DC_fp(LG_cardiac, cardiac_freq, TR);
-% [p2, l2] = self_gate_DC_fp( D_out(:,coil_ecg), freq_ecg, TR); close all
-% 
-% [pr1, lr1] = self_gate_DC_fp( smooth(LG_resp, d_inter), freq_resp, TR);
-% [pr2, lr2] = self_gate_DC_fp( smooth(D_out(:,coil_resp), d_inter), freq_resp, TR);
-
-%%
-% %
-% % figure, clear temp;
-% % for i = [1 11]
-% %     temp = mean(reshape(real(data(201:end,i)), [2 length(data(201:end,1))/2]),1);
-% %     subplot(1,2,1); hold on;
-% %     plot(smooth(real(temp)))
-% %
-% %     subplot(1,2,2); hold on;
-% %     Y = fft(smooth(real(temp)));
-% %     P2 = abs(Y/length(temp));
-% %     P1 = P2(1:(length(temp))/2+1);
-% %     P1(2:end-1) = 2*P1(2:end-1);
-% %     f = (1/TR)*(0:((length(temp))/2))/(length(temp));
-% %     plot(f, P1)
-% % end
-% %
-% % find(f > 1.72,1, 'first')
-% %
-% % P2 = P1;
-% % P2(29:end) =0;
-% % figure, plot(abs(ifft(P2)))
-% %
-% % [frequencies, dampings, basis, ahat] = hsvd_new(temp,55);
-% %
-% % dt = 1 /55;
-% % t = (0:dt:(length(temp)-1)*dt);
-% % tempf = find(0.5 < frequencies & frequencies < 3.5);
-% %
-% % figure,plot(abs(basis(:,tempf)*ahat(tempf)))
-% % a = findpeaks(abs(basis(:,tempf)*ahat(tempf)))
-% % figure
-% % [b,c] = findpeaks(smooth(temp));
-%
-%% Ipshitta
-
-% figure, clear x_vec;
-% for i = 1:channels
-%     temp = mean(reshape(abs(data(:,i)), [2 length(data)/2]),1);
-%     temp = temp./max(temp(:));
-%     x_vec(:,i) = temp;
-% end
-% plot(x_vec);
-% % Take SVD
-% [U,S,V] = svd(x_vec,'econ');
-% figure, plot(diag(S))
-% 
-% % Rank Contribution
-% rank_matrix = zeros(size(U));
-% for i = 1:length(S)
-%     rank_matrix(:,i) = U(:,i)*S(i,i)*V(i,i)';
-% end
-% figure, colorful_plots(rank_matrix)
-% 
-% figure, clear P_out D_out;
-% for i = 1:channels
-%     subplot(1,2,1); hold on;
-%     plot(smooth(rank_matrix(:,i)), '-', 'Color', cmp(i,:))
-% 
-%     subplot(1,2,2); hold on;
-%     Y = fft(smooth( rank_matrix(:,i) ));
-%     P2 = abs(Y/length(temp));
-%     P1 = P2(1:(length(temp))/2+1);
-%     P1(2:end-1) = 2*P1(2:end-1);
-%     f = (1/TR)*(0:((length(temp))/2))/(length(temp));
-%     plot(f, P1, '-', 'Color', cmp(i,:))
-% %     P_out(:,i) = P1;
-% end
-% xlim([0.1 3.6])
-% legend(leg_chan)
-% 
-% 
-% % Raj's PCA
-% pca_chan = 4;
-% figure, plot(U(:,1:pca_chan)*S(1:pca_chan,1:pca_chan)*V(1:pca_chan,1:pca_chan)')
-% 
-% % tructate SVD
-% 
-% rank_K = 5;
-% 
-% s_diag = diag(S);
-% s_diag_trunc = zeros(size(s_diag));
-% s_diag_trunc(1:rank_K) = s_diag(1:rank_K);
-% 
-% S_trunc = zeros(size(S));
-% 
-% S_trunc(1:size(x_vec,2),1:size(x_vec,2)) = diag(s_diag_trunc);
-% 
-% x_rec_vec = U*S_trunc*V';
-% figure, colorful_plots(x_rec_vec)
-% 
-% 
-% 
-% % X_rec = reshape(x_rec_vec,[size(x,1),size(x,2),size(x,3)]);
-% X_rec = x_rec_vec;
-% %  plot recon
-% figure;
-% 
-% slc = 1;
-% 
-% imagesc([x(:,:,slc),X_rec(:,:,slc)]);colormap(gray);
-% 
-% figure,plot(X_rec);
-% 
-% sp1 = floor(sqrt(double(channels)));sp2 = ceil(double(channels)/sp1);
-% 
-% figure,
-% for i = 1:double(channels)
-%     subplot(sp1,sp2,i); hold on;
-%     plot(x_vec(:,i), 'k-'),
-%     plot(x_rec_vec(:,i), 'r-')
-% end
-% figure, plot(U)
 
 end
 
