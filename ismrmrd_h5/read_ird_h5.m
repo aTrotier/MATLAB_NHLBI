dabd498bfd4c9a3c7a1568f438f560278a16d364
rajramasawmy <rajramasawmy@users.noreply.github.com>
2019-10-25 10:26:48 -0400

    generic read_ird_h5
    will read all groups, all images.

--- ismrmrd_h5/read_ird_h5.m
+++ ismrmrd_h5/read_ird_h5.m
@@ -1,62 +1,42 @@
-function [Mag_data, Pha_data, info] = read_ird_h5(filename)
-% [Mag_data, Pha_data, info] = read_ird_h5(filename)
-% Reads in an image from a Gadgetron Recon
+function [outputStruct] = read_ird_h5(filename)
+% [output_struct] = read_ird_h5(filename)
+% Reads in all images from an offline Gadgetron Recon
 %
-% This needs upgrading.. (can put data by group and in to structure)
-% atm ignores multiple recons in one dataset.. 
+% R Ramasawmy, NHLBI, Oct 2019
 
+% user UI if empty
 if nargin < 1
-   [a,b] = uigetfile('*.*');
-   filename = [b filesep a]; clear a b;
+    [a,b] = uigetfile('*.*');
+    filename = [b filesep a]; clear a b;
 end
 a = h5info(filename);
-b = a.Groups.Groups;
 
-m_ind = 0;
-p_ind = 0;
-for i = 1 : length(b)
-    temp = b(i).Name;
-    disp(['Group ' num2str(i) ': ' temp]);
-    ind = regexp(temp, 'image_');
-    lnum(i) = length(temp(ind:end));
+%%
+% check the number of reconstructions
+numRecons = length(a.Groups); disp(' '); disp('===================================='); disp(' ');
+for iNR = 1:numRecons
     
-end
+    disp(['Loading Image set: ' num2str(iNR) '   Recon ID: ' a.Groups(iNR).Name]); disp(' ');
+    outputStruct.(matlab.lang.makeValidName(['group_' num2str(iNR)])).id = a.Groups(iNR).Name;
 
-m_s_len = min(lnum);
-p_s_len = max(lnum);
-
-for i = 1 : length(b)
-    temp = b(i).Name;
+    % optional ismrmrd header attachment
+    % info = h5read(filename, [temp '/header']);
+    % outputStruct.(matlab.lang.makeValidName(['group_' num2str(iNR)])).info = info;
     
-    if(length(unique(lnum)) > 1) % if phase images are present
+    % grab all the data in this recon group
+    b = a.Groups(iNR).Groups;
+    for i=1:length(b)
+        temp = b(i).Name;
+        ind = regexp(temp, 'image_'); % just take the image ID
+        image_name = temp(ind:end); disp(temp)
         
-        if lnum(i) == m_s_len
-            % Magnitude Image
-            m_ind = m_ind + 1;
-            Mag_data(:,:,:,m_ind) = squeeze(double(h5read(filename, [temp '/data'])));
-            
-            if m_ind == 1
-                info = h5read(filename, [temp '/header']);
-            end
-            
-        elseif lnum(i) == p_s_len
-            % Phase Image
-            
-            p_ind = p_ind + 1;
-            Pha_data(:,:,:,p_ind) = squeeze(double(h5read(filename, [temp '/data'])));
-            
-        end
+         data_temp = squeeze(double(h5read(filename, [temp '/data']))); % simplify for image proc
+         outputStruct.(matlab.lang.makeValidName(['group_' num2str(iNR)])).(matlab.lang.makeValidName(image_name)) = data_temp;
         
-    else % Just magnitude
-        m_ind = m_ind + 1;
-        Mag_data(:,:,:,m_ind) = squeeze(double(h5read(filename, [temp '/data'])));
-        
-        if m_ind == 1
-            info = h5read(filename, [temp '/header']);
-        end
-        
-        Pha_data = [];
     end
+    
+    disp(' '); disp('===================================='); disp(' ');
+    
 end
 
 
